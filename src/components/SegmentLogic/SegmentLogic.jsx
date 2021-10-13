b6358d758c2a4d992fcc43707767ea3930bc09ae
Rick Makkee <rick@mediamonks.com>
2021-10-12 17:45:29 -0700

    Add base layout with mock data

---
+++ src/components/SegmentLogic/SegmentLogic.jsx
@@ -0,0 +1,74 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+import React from 'react'
+import { Box2, Span } from '@looker/components'
+import { Filter } from '@looker/filter-components'
+import PropTypes from 'prop-types'
+
+export const SegmentLogic = ({ activeFilters }) => {
+  return (
+    <div>
+      {activeFilters.map((filter) => (
+        <Box2 key={filter.id} mt="u5">
+          <Span fontSize="small">{filter.label}</Span>
+
+          {filter.type === 'string' && (
+            <Filter
+              name={filter.label}
+              type="field_filter"
+              expressionType="string"
+              expression=""
+            />
+          )}
+          {filter.type === 'number' && (
+            <Filter
+              name={filter.label}
+              type="field_filter"
+              expressionType="number"
+              expression=""
+            />
+          )}
+          {filter.type === 'date_time' && (
+            <Filter
+              name={filter.label}
+              expressionType="date_time"
+              expression="1 week"
+            />
+          )}
+          {filter.type === 'location' && (
+            <Filter
+              name={filter.label}
+              expressionType="location"
+              expression="36.9741, 122.0308"
+            />
+          )}
+        </Box2>
+      ))}
+    </div>
+  )
+}
+
+SegmentLogic.propTypes = {
+  activeFilters: PropTypes.array,
+}
